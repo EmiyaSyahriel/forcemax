7272c12b4d6bafece88998c555d2db98ca1e2c1e
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2024-03-20 21:55:25 +0700

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # forcemax
- Force Windows window to maximize
+Force Windows window to maximize and additionally make it borderless, useful for games that has it's maximize button is disabled e.g DMM version of デレステ
+
+## Features
+- Make window fullscreen
+- Make window borderless
+- Return the window border (not guaranteed to be the same border, just works for the most)
+
+## Usage
+- `-h, --help`<br/>
+    Show help
+
+- `-c, --class-name [window_class_name=null]`<br/>
+    Select by window class name
+
+- `-n, --window-name` [window_name=null]<br/>
+    Select by window name / caption / title
+
+- `-h, --handle-window` [hwnd=null]<br/>
+    Select by window handle (HWND)
+
+- `-r, --restore`<br/>
+    Restore window
+
+- `-M, --maximize`<br/>
+    Maximize window
+
+- `-b, --borderless`<br/>
+    Make window borderless
+
+- `-B, --bordered`<br/>
+    Restore window border
+
+- `-C, --corner`<br/>
+    Move window to top-left of monitor (sometimes not working)
+
+> If the target window is elevated / launched using Run As Administrator (e.g デレステ DMM ver.), this program
+> will also needs to be run on elevated environment (e.g `cmd` that is launched using Run as Administrator)
+> or otherwise no effect will be working.
+
+## Building
+see [Building](BUILDING.MD)
+
+## License
+Creative Commons Attribution 4.0, see [LICENSE](LICENSE.MD)
---
+++ src/forcemax.c
@@ -0,0 +1,149 @@
+#define WINDOWS_LEAN_AND_MEAN
+#include <windows.h>
+
+LPCSTR help = 
+	"Usage: forcemax.exe [args ...]\n"
+	"\n"
+	"Arguments:\n"
+	"\t-h, --help\n"
+	"\t    Show this help\n"
+	"\t\n"
+	"\t-c, --class-name [window_class_name=null]\n"
+	"\t    Select by window class name\n"
+	"\t\n"
+	"\t-n, --window-name [window_name=null]\n"
+	"\t    Select by window name / caption / title\n"
+	"\t\n"
+	"\t-h, --handle-window [hwnd=null]\n"
+	"\t    Select by window handle (HWND)\n"
+	"\t\n"
+	"\t-r, --restore\n"
+	"\t    Restore window\n"
+	"\t\n"
+	"\t-M, --maximize\n"
+	"\t    Maximize window\n"
+	"\t\n"
+	"\t-b, --borderless\n"
+	"\t    Make window borderless\n"
+	"\t\n"
+	"\t-B, --bordered\n"
+	"\t    Restore window border\n"
+	"\t\n"
+	"\t-C, --corner\n"
+	"\t    Move window to top-left of monitor (sometimes not working)\n"
+	"\n"
+;
+
+LPCSTR err_nowindow =
+	"Window not found"
+;
+
+int main(INT32 argc, LPCSTR argv[]) {
+
+	BOOLEAN restore     = FALSE;
+	BOOLEAN maximize    = FALSE;
+	BOOLEAN borderless  = FALSE;
+	BOOLEAN bordered    = FALSE;
+	BOOLEAN corner      = FALSE;
+	LPCSTR wndClass     = NULL;
+	LPCSTR wndCapt      = NULL;
+	LPCSTR handleWindow = NULL;
+	HWND hWnd           = NULL;
+	INT32 written       = 0;
+	HANDLE stdOut       = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	// Parse Parameters
+	for (INT32 i = 1; i < argc; i++) {
+		LPCSTR v = argv[i];
+		if (lstrcmp(v, "--help") == 0 || lstrcmp(v, "-h") == 0) {
+			WriteConsole(stdOut, help, lstrlen(help), &written, NULL);
+			return 0;
+		}
+		else if ((lstrcmp(v, "--class-name") == 0 || lstrcmp(v, "-c") == 0) && i + 1 < argc) {
+			wndClass = argv[++i];
+		}
+		else if ((lstrcmp(v, "--window-name") == 0 || lstrcmp(v, "-n") == 0) && i + 1 < argc) {
+			wndCapt = argv[++i];
+		}
+		else if ((lstrcmp(v, "--handle-window") == 0 || lstrcmp(v, "-h") == 0) && i + 1 < argc) {
+			handleWindow = argv[++i];
+		}
+		else if (lstrcmp(v, "--restore") == 0 || lstrcmp(v, "-r") == 0) {
+			restore = TRUE;
+		}
+		else if (lstrcmp(v, "--maximize") == 0 || lstrcmp(v, "-M") == 0) {
+			maximize = TRUE;
+		}
+		else if (lstrcmp(v, "--borderless") == 0 || lstrcmp(v, "-b") == 0) {
+			borderless = TRUE;
+		}
+		else if (lstrcmp(v, "--bordered") == 0 || lstrcmp(v, "-B") == 0) {
+			bordered = TRUE;
+		}
+		else if (lstrcmp(v, "--corner") == 0 || lstrcmp(v, "-C") == 0) {
+			corner = TRUE;
+		}
+	}
+
+	if (!(wndClass != NULL || wndCapt != NULL || handleWindow != NULL) || argc <= 1) {
+		WriteConsole(stdOut, help, lstrlen(help), &written, NULL);
+		return 1;
+	}
+
+	if (handleWindow != NULL) {
+		hWnd = (HWND)strtol(handleWindow, NULL, NULL) ;
+	}
+
+	if (hWnd == NULL) {
+		hWnd = FindWindow(wndClass, wndCapt);
+	}
+
+	if (hWnd == NULL) {
+		WriteConsole(stdOut, err_nowindow, lstrlen(err_nowindow), &written, NULL);
+		return 1;
+	}
+
+	if (corner) {
+		HMONITOR hMon = MonitorFromWindow(hWnd, MONITOR_DEFAULTTOPRIMARY);
+		MONITORINFOEX hMI;
+		ZeroMemory(&hMI, sizeof(MONITORINFOEX));
+		hMI.cbSize = sizeof(MONITORINFOEX);
+		INT32 x, y, w, h, move;
+		if (move = GetMonitorInfo(hMon, &hMI)) {
+			x = hMI.rcMonitor.left;
+			y = hMI.rcMonitor.top;
+			w = hMI.rcMonitor.right - hMI.rcMonitor.left;
+			h = hMI.rcMonitor.bottom - hMI.rcMonitor.top;
+		}
+		if (move) SetWindowPos(hWnd, NULL, x, y, w, h, SWP_ASYNCWINDOWPOS | SWP_NOZORDER);
+	}
+
+	if (maximize || restore) {
+		if (corner) {
+			Sleep(500);
+		}
+
+		if (maximize) {
+			PostMessage(hWnd, WM_SYSCOMMAND, SC_MAXIMIZE, 0);
+		}
+		if (restore) {
+			PostMessage(hWnd, WM_SYSCOMMAND, SC_RESTORE, 0);
+		}
+	}
+
+	if (bordered || borderless) {
+		if (maximize || restore || corner) { // If setting border, wait for 500ms, for some reason window to actually maximized
+			Sleep(500);
+		}
+		LONG lStyle = GetWindowLong(hWnd, GWL_STYLE);
+		if (bordered) {
+			lStyle |= (WS_CAPTION | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU);
+		}
+		else if(borderless) {
+			lStyle &= ~(WS_CAPTION | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU);
+		}
+		SetWindowLong(hWnd, GWL_STYLE, lStyle);
+	}
+
+	return 0;
+}
